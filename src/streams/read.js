d1647c77d23f465e103ffaafd4e2429688126639
Nikita Mikhalevich <n.mikhalevich@syberry.com>
2023-06-19 19:43:57 +0300

    feat: Done streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import process from 'node:process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(__dirname + '/files/fileToRead.txt');
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,4 +1,14 @@
+import {Transform} from 'stream';
+
 const transform = async () => {
+    const transformStream = new Transform ({
+        transform: (data, enc, callback) => {
+            const reverseData = [...data.toString()].reverse().join('');
+            callback(null, reverseData);
+        }
+    });
+
+    process.stdin.pipe(transformStream).pipe(process.stdout);
     // Write your code here 
 };
 
