99d6a90bbc7e8fb17fffc75f458b74a5f74a3703
Nikita Mikhalevich <n.mikhalevich@syberry.com>
2023-06-19 19:43:13 +0300

    feat: Done hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const hash = crypto.createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+    fs.readFile(__dirname + '/files/fileToCalculateHashFor.txt', 'utf-8', (err, data) => {
+        if (err) {
+            return;
+        }
+        
+        hash.update(data);
+        return console.log(hash.digest('hex'));  
+    })
 };
 
 await calculateHash();
